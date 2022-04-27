9289b54ea140434b0e9102e93230fae6c038f0be
zearkiatos <caprilespe@outlook.com>
2022-04-27 19:22:04 -0400

    Implementing test for boolean and array

---
+++ README.md
@@ -0,0 +1,6 @@
+# Description
+This is a project with a base configuration with jest to practice and improvement skills
+
+## Make With
+[![JavaScript](https://img.shields.io/badge/javascript-ead547?style=for-the-badge&logo=javascript&logoColor=white&labelColor=000000)]()
+[![Jest](https://img.shields.io/badge/jest-bc3913?style=for-the-badge&logo=jest&logoColor=white&labelColor=000000]()
--- tests/global.test.js
+++ tests/global.test.js
@@ -5,4 +5,37 @@ describe('This is a global test for jest', () => {
         
         expect(text).toMatch(/World/)
     });
+
+    test('Should content an array', () => {
+        const fruits = ['apples', 'melon', 'banana'];
+
+        expect(fruits).toContain('banana');
+    });
+
+    test('Should evaluate if a number is grant to other', () => {
+        const TEN = 10;
+        const NINE = 9;
+
+        expect(TEN).toBeGreaterThan(NINE);
+    });
+
+    test('Should be a true value', () => {
+        const trueValue = true;
+
+        expect(trueValue).toBeTruthy();
+    });
+
+    test('Should reverse a string callback', () => {
+        const resultExpected = "!dlroW olleH";
+        const reverseString = (str, callback) => {
+            callback(str.split("").reverse().join(""));
+        };
+
+        reverseString("Hello World!", (str) => {
+            expect(str).toBe(resultExpected);
+        });
+    });
+
+
+
 });
